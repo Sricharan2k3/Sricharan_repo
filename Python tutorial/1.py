8939eaef4586c2eeaa8b4d3be30a5744b881336f
N SRICHARAN <namasricharan@gmail.com>
2022-06-12 22:22:16 +0530

    Add more elements to dictionary

--- Python tutorial/1.py
+++ Python tutorial/1.py
@@ -10,3 +10,8 @@ for rollno, details in dict.items():
     s=s+float(details[1])
 k=round(s/7,2)
 print("Average percentage of students is" , k)
+# appending new key values to dictionary
+dict[119]=["Ganesh","93","8"]
+for rollno, details in sorted(dict.items()):
+    # sorted function is used to sord the key values from ascending to descending
+    print(rollno, ':', details)
