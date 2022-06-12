afa624f3e4675ab77197614c2ce52acfe20ecb55
N SRICHARAN <namasricharan@gmail.com>
2022-06-12 22:57:04 +0530

    Vowel check using membership operator

---
+++ Python tutorial/vowel check.py
@@ -0,0 +1,14 @@
+# vowel check using membership operator
+alphabet= input("Enter any alphabet: ").lower()
+if   ("a" in alphabet):
+    print(f"{alphabet} is a vowel")
+elif ("e" in alphabet):
+    print(f"{alphabet} is a vowel")
+elif ("i" in alphabet):
+    print(f"{alphabet} is a vowel")
+elif ("o" in alphabet):
+    print(f"{alphabet} is a vowel")
+elif ("o" in alphabet):
+    print(f"{alphabet} is a vowel")
+else
+    print(f"{alphabet} is a consonant")
